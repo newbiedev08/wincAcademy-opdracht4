37a951297beb6166419fced7d7844bc73ff52a57
Ivy Kathrina <88631468+newbiedev08@users.noreply.github.com>
2021-10-21 18:44:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+//Deel 1 objecten;
+let person = {
+    name: 'Ivy',
+    age: 37
+};
+console.log(person);
+
+let name = 'Ivy';
+console.log(name);
+let age = 37;
+console.log(37);
+
+//Dot Notation//
+const age = person.age;
+person.age = 37;
+console.log(person.age);
+
+//Braket Notation//
+const person - {};
+person['name'] = 'Ivy';
+console.log(person.name);
+
+//evaluation//
+let evaluations = 7 + 10 + 9;
+console.log(evaluations);
+
+
+//Array//
+let evaluations = ['7', '10', '9'];
+evaluations[3] = 1;
+console.log(evaluations.length);
+
+//Deel 2 Arrays//
+const array = ['groen', 'blauw', 'rood'];
+console.log(array);
+
+const kleur = ['groen', 'blauw', 'rood'];
+console.log(kleur.length);
+console.log(array[0]);
+console.log(array[2]);
+ //Add geel//
+const kleur = ['groen', 'blauw', 'rood'];
+kleur[3] ='geel';
+console.log(kleur);
+
+//add 5//
+
+const kleur = ['groen', 'blauw', 'rood'];
+kleur[3]='geel';
+kleur[4]=5;
+console.log(kleur);
+//add object greeting//
+const kleur = ['groen', 'blauw', 'rood'];
+kleur[3, 4, 5] = 'geel', 5,'Hi ik ben een object ';
+console.log(kleur);
+
+console.log("Herkomst kat 1: ", catBreeds[0] origin)
+console.log("soort kat 3: ", catBreeds[2].name)
+console.log("energy kat 1: ", catBreeds[0].energy_level)
+console.log("temper kat 2: ", catBreeds[1].temperament[0])
+console.log("temper kat 3: ", catBreeds[2].temperament[4])
+console.log("food kat 3: ", catBreeds[2].food_favourite_food);
